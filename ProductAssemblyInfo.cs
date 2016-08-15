75c1ef0f12c994db9e9eabc1506a6f083742c9e5
Віктор Айтішник <it4history@mail.ru>
2016-08-15 16:44:14 +0300

    Add files via upload

---
+++ ProductAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyVersionAttribute(Globe.Version)]
+[assembly: AssemblyFileVersionAttribute(Globe.Version)]
+[assembly: AssemblyCompany("Skills.gq")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+
+public class Globe
+{
+    public const string Version = "1.0.0.1";
+}
