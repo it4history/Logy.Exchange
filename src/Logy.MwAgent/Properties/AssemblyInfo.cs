2e352990293e9b8b5d1045efb6f9054d0df663d8
Віктор Айтішник <it4history@mail.ru>
2016-08-15 18:57:14 +0300

    Create AssemblyInfo.cs

---
+++ src/Logy.MwAgent/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Logy.MwAgent")]
+[assembly: AssemblyDescription("Mediawiki agent")]
+[assembly: AssemblyProduct("Logy.MwAgent")]
+
+[assembly: Guid("cfca8758-05ca-4f87-9edf-5f390aa28c8c")]
